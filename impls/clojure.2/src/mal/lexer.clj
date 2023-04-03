26fcd5fb1119b5dfc33283a6c61c601ea806dd8f
Andrey Krainyak <mode.andrew@gmail.com>
2023-04-03 21:59:41 +0800

    feat[clj2]: lexer

---
+++ impls/clojure.2/src/mal/lexer.clj
@@ -0,0 +1,167 @@
+(ns mal.lexer
+  (:refer-clojure :exclude [atom comment newline symbol])
+  (:require [mal.parsing :as pa])
+  (:import [mal.parsing Value]))
+
+(defrecord Token [value line column])
+(defrecord Comment [text])
+(defrecord Symbol [name])
+
+(defn letter? [ch]
+  (java.lang.Character/isLetter ch))
+
+(defn digit? [ch]
+  (java.lang.Character/isDigit ch))
+
+(defn run [parser char-stream]
+  (let [result (pa/run parser [char-stream 1 1])
+        [stream line column] (:state result)]
+    (merge
+      (if (instance? Value result)
+        {:value (:value result)}
+        {:error (:message result)})
+      {:remainder stream
+       :line line
+       :column column})))
+
+(def any-char
+  (pa/make-parser [[stream line column :as state]]
+    (if (empty? stream)
+      (pa/->ParseError "any character" state)
+      (let [ch (first stream)]
+        (pa/->Value ch [(rest stream)
+                        (if (= ch \newline) (inc line) line)
+                        (if (= ch \newline) 1 (inc column))])))))
+
+(def end-of-stream
+  (pa/make-parser [[stream line column :as state]]
+    (if (empty? stream)
+      (pa/->Value :end-of-stream [nil line column])
+      (pa/->ParseError "end of stream" state))))
+
+(defn character [ch]
+  (pa/label (str "'" ch "'")
+    (pa/try
+      (pa/satisfy
+        (fn [x] (= x ch))
+        any-char))))
+
+(defn one-of [chs]
+  (let [chars-set (set (seq chs))]
+    (pa/label (str "one of '" chs "' characters")
+      (pa/try
+        (pa/satisfy
+          (fn [ch] (contains? chars-set ch))
+          any-char)))))
+
+(def space (character \space))
+(def newline (character \newline))
+(def tab (character \tab))
+(def comma (character \,))
+(def double-quote (character \"))
+(def backslash (character \\ ))
+(def semicolon (character \;))
+(def tilde (character \~))
+(def at (character \@))
+
+(def whitespace
+  (pa/label "whitespace"
+    (pa/choice space newline tab comma)))
+
+(def whitespaces
+  (pa/skip-many whitespace))
+
+(def comment
+  (pa/let-bind
+    [_ semicolon
+     text (pa/many any-char :till (pa/choice newline end-of-stream))]
+    (pa/return (->Comment (apply str text)))))
+
+(def escape-sequence
+  (pa/label "escape sequence"
+    (pa/let-bind [_ backslash
+                  c (pa/choice
+                      (pa/map (character \n) :to \newline)
+                      (pa/map (character \t) :to \tab)
+                      (character \\ )
+                      (character \" ))]
+      (pa/return c))))
+
+(def string-literal
+  (pa/label "string literal"
+    (pa/let-bind
+      [_ double-quote
+       cs (pa/many (pa/choice escape-sequence any-char)
+            :till double-quote)]
+      (pa/return (apply str cs)))))
+
+(def special-character
+  (pa/label "special character"
+    (pa/choice
+      (pa/map (pa/sequence [tilde at]) :to "~@")
+      (one-of "[]{}()'`~^@"))))
+
+(def letter
+  (pa/label "letter"
+    (pa/try (pa/satisfy letter? any-char))))
+
+(def digit
+  (pa/label "digit"
+    (pa/try (pa/satisfy digit? any-char))))
+
+(def symbol-init-char
+  (pa/choice
+    letter
+    (one-of "*+!-_'?<>=/.")))
+
+(def symbol-rest-char
+  (pa/choice
+    symbol-init-char
+    digit))
+
+(def symbol
+  (pa/label "symbol"
+    (pa/let-bind [init-char symbol-init-char
+                  rest-chars (pa/many symbol-rest-char)]
+      (pa/return (->Symbol (apply str (cons init-char rest-chars)))))))
+
+(defn integer-from-string [digits]
+  (try
+    (Integer/parseInt digits)
+    (catch Exception _
+      nil)))
+
+(def integer
+  (pa/let-bind [digits (pa/many digit)
+                _ (pa/not-followed-by symbol-rest-char)]
+    (let [number (integer-from-string (apply str digits))]
+      (if (some? number)
+        (pa/return number)
+        (pa/fail "integer")))))
+
+(def number
+  (pa/label "number"
+    (pa/choice integer)))
+
+(def token-types
+  [special-character
+   symbol
+   comment
+   string-literal
+   number])
+
+(def token
+  (pa/make-parser [state]
+    (let [parser (pa/let-bind [value (apply pa/choice token-types)
+                               _ whitespaces]
+                   (pa/return
+                     (let [[_ line column] state]
+                       (->Token value line column))))]
+      (pa/run parser state))))
+
+(defn tokenize [stream]
+  (-> (pa/let-bind [_ whitespaces
+                    parsed-tokens (pa/many token :till end-of-stream)]
+        (pa/return parsed-tokens))
+      (run stream)
+      :value))
---
+++ impls/clojure.2/test/mal/lexer_test.clj
@@ -0,0 +1,167 @@
+(ns mal.lexer-test
+  (:refer-clojure :exclude [comment])
+  (:require [clojure.string :refer [join]]
+            [clojure.test :refer [deftest is]]
+            [mal.lexer :as l]))
+
+(deftest any-char
+  (let [p (partial l/run l/any-char)]
+    (is (= (p "") {:error "any character"
+                   :remainder "" :line 1 :column 1}))
+    (is (= (p "1") {:value \1 :remainder '() :line 1 :column 2}))
+    (is (= (p "\n1") {:value \newline :remainder '(\1) :line 2 :column 1}))))
+
+(deftest end-of-stream
+  (let [p (partial l/run l/end-of-stream)]
+    (is (= (p "") {:value :end-of-stream :remainder nil :line 1 :column 1}))
+    (is (= (p "1") {:error "end of stream"
+                    :remainder "1" :line 1 :column 1}))))
+
+(deftest character
+  (let [p (partial l/run (l/character \1))]
+    (is (= (p "1") {:value \1 :remainder '() :line 1 :column 2}))
+    (is (= (p "2") {:error "'1'" :remainder "2" :line 1 :column 1}))))
+
+(deftest one-of
+  (let [p (partial l/run (l/one-of "123"))]
+    (is (= (p "1") {:value \1 :remainder '() :line 1 :column 2}))
+    (is (= (p "2") {:value \2 :remainder '() :line 1 :column 2}))
+    (is (= (p "3") {:value \3 :remainder '() :line 1 :column 2}))
+    (is (= (p "4") {:error "one of '123' characters"
+                    :remainder "4" :line 1 :column 1}))))
+
+(deftest whitespace
+  (let [p (partial l/run l/whitespace)]
+    (is (= (p " ") {:value \space :remainder '() :line 1 :column 2}))
+    (is (= (p ",") {:value \, :remainder '() :line 1 :column 2}))
+    (is (= (p "\t") {:value \tab :remainder '() :line 1 :column 2}))
+    (is (= (p "\n") {:value \newline :remainder '() :line 2 :column 1}))
+    (is (= (p "1") {:error "whitespace"
+                    :remainder "1" :line 1 :column 1}))))
+
+(deftest whitespaces
+  (let [p (partial l/run l/whitespaces)]
+    (is (= (p " ") {:value nil :remainder '() :line 1 :column 2}))
+    (is (= (p "  ") {:value nil :remainder '() :line 1 :column 3}))
+    (is (= (p "  1") {:value nil :remainder '(\1) :line 1 :column 3}))
+    (is (= (p " \n") {:value nil :remainder '() :line 2 :column 1}))
+    (is (= (p " \n\t") {:value nil :remainder '() :line 2 :column 2}))
+    (is (= (p "\t\n \t,\t ,") {:value nil :remainder '() :line 2 :column 7}))
+    (is (= (p ", \n ,\t \n,\t")
+           {:value nil :remainder '() :line 3 :column 3}))))
+
+(deftest comment
+  (let [p (partial l/run l/comment)]
+    (is (= (p ";") {:value (l/->Comment "")
+                    :remainder nil :line 1 :column 2}))
+    (is (= (p ";\n") {:value (l/->Comment "")
+                      :remainder '() :line 2 :column 1}))
+    (is (= (p ";1") {:value (l/->Comment "1")
+                     :remainder nil :line 1 :column 3}))
+    (is (= (p ";12") {:value (l/->Comment "12")
+                      :remainder nil :line 1 :column 4}))
+    (is (= (p ";123\n") {:value (l/->Comment "123")
+                         :remainder '() :line 2 :column 1}))))
+
+(deftest escape-sequence
+  (let [p (partial l/run l/escape-sequence)]
+    (is (= (p [\\ \n]) {:value \newline :remainder '() :line 1 :column 3}))
+    (is (= (p [\\ \t]) {:value \tab :remainder '() :line 1 :column 3}))
+    (is (= (p [\\ \"]) {:value \" :remainder '() :line 1 :column 3}))
+    (is (= (p "\\\\") {:value \\ :remainder '() :line 1 :column 3}))
+    (is (= (p [\\ \1]) {:error "escape sequence"
+                        :remainder '(\1) :line 1 :column 2}))))
+
+(deftest string-literal
+  (let [p (partial l/run l/string-literal)]
+    (is (= (p [\" \"]) {:value "" :remainder '() :line 1 :column 3}))
+    (is (= (p [\"])
+           {:error "string literal" :remainder '() :line 1 :column 2}))
+    (is (= (p [\" \1 \"]) {:value "1" :remainder '() :line 1 :column 4}))
+    (is (= (p [\" \1 \newline \2 \"])
+           {:value "1\n2" :remainder '() :line 2 :column 3}))
+    (is (= (p [\" \\ \" \"]) {:value "\"" :remainder '() :line 1 :column 5}))
+    (is (= (p [\" \\ \n \"]) {:value "\n" :remainder '() :line 1 :column 5}))
+    (is (= (p [\" \1 \\ \" \2 \\ \\ \\ \n \3 \\ \\ \\ \" \4 \"])
+           {:value (str \1 \" \2 \\ \newline \3 \\ \" \4)
+            :remainder '() :line 1 :column 17}))))
+
+(deftest letter
+  (let [p (partial l/run l/letter)]
+    (is (= (p "a") {:value \a :remainder '() :line 1 :column 2}))
+    (is (= (p "1") {:error "letter" :remainder "1" :line 1 :column 1}))))
+
+(deftest digit
+  (let [p (partial l/run l/digit)]
+    (is (= (p "1") {:value \1 :remainder '() :line 1 :column 2}))
+    (is (= (p "a") {:error "digit" :remainder "a" :line 1 :column 1}))))
+
+(deftest integer
+  (let [p (partial l/run l/integer)]
+    (is (= (p "1") {:value 1 :remainder '() :line 1 :column 2}))
+    (is (= (p "12345") {:value 12345 :remainder '() :line 1 :column 6}))
+    (is (= (p "12345a") {:error "wrong input"
+                         :remainder '(\a) :line 1 :column 6}))
+    (is (= (p "12345)") {:value 12345
+                         :remainder '( \) ) :line 1 :column 6}))
+    (is (= (p "2147483647") {:value 2147483647
+                             :remainder '() :line 1 :column 11}))
+    (is (= (p "2147483648") {:error "integer"
+                             :remainder '() :line 1 :column 11}))))
+
+(deftest symbols
+  (let [p (partial l/run l/symbol)]
+    (is (= (p "a") {:value (l/->Symbol "a")
+                    :remainder '() :line 1 :column 2}))
+    (is (= (p "a1") {:value (l/->Symbol "a1")
+                     :remainder '() :line 1 :column 3}))
+    (is (= (p "*s+o!m-e_s'y?m<b>o=l/n.a1m2e")
+           {:value (l/->Symbol "*s+o!m-e_s'y?m<b>o=l/n.a1m2e")
+            :remainder '() :line 1 :column 29}))))
+
+(deftest token
+  (let [p (partial l/run l/token)]
+    (is (= (p "~@ ,\n") {:value (l/->Token "~@" 1 1)
+                         :remainder '() :line 2 :column 1}))
+    (is (= (p "some-symbol\n\n\t")
+           {:value (l/->Token (l/->Symbol "some-symbol") 1 1)
+            :remainder '() :line 3 :column 2}))
+    (is (= (p "; some comment\n,\n\t\n")
+           {:value (l/->Token (l/->Comment " some comment") 1 1)
+            :remainder '() :line 4 :column 1}))
+    (is (= (p "\"some\\\"st\nring\"\t, \n, ")
+           {:value (l/->Token "some\"st\nring" 1 1)
+            :remainder '() :line 3 :column 3}))
+    (is (= (p "1234, ") {:value (l/->Token 1234 1 1)
+                         :remainder '() :line 1 :column 7}))
+    (is (= (p "42x ") {:error "number",
+                       :remainder '(\x \space) :line 1 :column 3}))))
+
+(deftest tokenize
+  (is (= (l/tokenize "") []))
+  (is (= (l/tokenize " \n\t ") []))
+  (is (= (l/tokenize " ,; some comments \n\t,")
+         [(l/->Token (l/->Comment " some comments ") 1 3)]))
+  (is (= (l/tokenize
+           (join \newline
+             ["  ; some tokens"
+              "(a 'b {c d,"
+              "       e [f g]"
+              "       \"h\" 42})"]))
+         [(l/->Token (l/->Comment " some tokens") 1 3)
+          (l/->Token \( 2 1)
+          (l/->Token (l/->Symbol "a") 2 2)
+          (l/->Token \' 2 4)
+          (l/->Token (l/->Symbol "b") 2 5)
+          (l/->Token \{ 2 7)
+          (l/->Token (l/->Symbol "c") 2 8)
+          (l/->Token (l/->Symbol "d") 2 10)
+          (l/->Token (l/->Symbol "e") 3 8)
+          (l/->Token \[ 3 10)
+          (l/->Token (l/->Symbol "f") 3 11)
+          (l/->Token (l/->Symbol "g") 3 13)
+          (l/->Token \] 3 14)
+          (l/->Token "h" 4 8)
+          (l/->Token 42 4 12)
+          (l/->Token \} 4 14)
+          (l/->Token \) 4 15)])))
