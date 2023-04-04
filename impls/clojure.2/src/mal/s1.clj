64f891de99e710b97ca218653080cd5d95eb4c1f
Andrey Krainyak <mode.andrew@gmail.com>
2023-04-04 23:42:32 +0800

    feat[clj2]: step 1

---
+++ impls/clojure.2/src/mal/s1.clj
@@ -0,0 +1,29 @@
+(ns mal.s1
+  (:require [mal.printer :as printer]
+            [mal.reader :as reader]))
+
+(defn READ [input]
+  (reader/read-string input))
+
+(defn EVAL [input]
+  input)
+
+(defn PRINT [input]
+  (printer/pr-str input))
+
+(defn rep [input]
+  (-> input
+      READ
+      EVAL
+      PRINT))
+
+(defn main [& _]
+  (loop []
+    (print "user> ")
+    (flush)
+    (when-let [input (read-line)]
+      (try
+        (-> input rep println)
+        (catch Exception e
+          (println "Error: " (.getMessage e))))
+      (recur))))
