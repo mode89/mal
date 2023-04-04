fec42b1ed32b4b9005a4864a8310ebc60c7c25b1
Andrey Krainyak <mode.andrew@gmail.com>
2023-04-04 22:24:11 +0800

    feat[clj2]: implement `pr-str`

---
+++ impls/clojure.2/src/mal/printer.clj
@@ -0,0 +1,26 @@
+(ns mal.printer
+  (:refer-clojure :exclude [pr-str])
+  (:require [clojure.string :refer [join]]
+            [mal.lexer])
+  (:import [mal.lexer Symbol]))
+
+(defn pr-char [ch]
+  (case ch
+    \"       "\\\""
+    \newline "\\n"
+    \tab     "\\t"
+    \\       "\\\\"
+    ch))
+
+(defn pr-str [object]
+  (cond
+    (number? object)
+      (str object)
+    (string? object)
+      (apply str (concat [\"] (map pr-char object) [\"]))
+    (instance? Symbol object)
+      (str (:name object))
+    (list? object)
+      (str \( (join " " (map pr-str object)) \) )
+    (vector? object)
+      (str \[ (join " " (map pr-str object)) \] )))
---
+++ impls/clojure.2/test/mal/printer_test.clj
@@ -0,0 +1,16 @@
+(ns mal.printer-test
+  (:require [clojure.test :refer [deftest is]]
+            [mal.lexer :as l]
+            [mal.printer :as p]))
+
+(deftest mal-pr-str
+  (is (= (p/pr-str 1) "1"))
+  (is (= (p/pr-str "1") "\"1\""))
+  (is (= (p/pr-str (l/->Symbol "a")) "a"))
+  (is (= (p/pr-str [1 "2" (l/->Symbol "x")]) "[1 \"2\" x]"))
+  (is (= (p/pr-str (list 1 ["2" (l/->Symbol "some-symbol")]))
+         "(1 [\"2\" some-symbol])"))
+  (is (= (p/pr-str "abc\"def") "\"abc\\\"def\""))
+  (is (= (p/pr-str "a\nb") "\"a\\nb\""))
+  (is (= (p/pr-str "a\tb") "\"a\\tb\""))
+  (is (= (p/pr-str "a\\b") "\"a\\\\b\"")))
