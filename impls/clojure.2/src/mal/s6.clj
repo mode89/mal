a6c2d82bb04fe28821ac6f674940d726f5b33a6e
Andrey Krainyak <mode.andrew@gmail.com>
2023-04-12 23:33:08 +0800

    feat[clj2]: step 6

---
+++ impls/clojure.2/src/mal/s6.clj
@@ -0,0 +1,47 @@
+(ns mal.s6
+  (:require [mal.core :as core]
+            [mal.environ :as environ]
+            [mal.reader :as reader]))
+
+(def repl-env
+  (environ/make
+    nil
+    core/core-ns))
+(environ/set! repl-env (core/symbol "eval")
+  (fn [form]
+    (core/eval form repl-env)))
+(environ/set! repl-env (core/symbol "load-file")
+  (fn [filename]
+    (core/eval
+      (core/read-string
+        (core/str "(do " (core/slurp filename) "\n" "nil" ")"))
+      repl-env)))
+
+(defn READ [input]
+  (reader/read-string input))
+
+(defn EVAL [form env]
+  (core/eval form env))
+
+(defn PRINT [input]
+  (core/pr-str input))
+
+(defn rep [input]
+  (-> input
+      READ
+      (EVAL repl-env)
+      PRINT))
+
+(defn -main [& args]
+  (environ/set! repl-env (core/symbol "*ARGV*") (apply list (rest args)))
+  (when-some [filename (first args)]
+    (rep (core/str "(load-file \"" filename "\")")))
+  (loop []
+    (print "user> ")
+    (flush)
+    (when-let [input (read-line)]
+      (try
+        (-> input rep println)
+        (catch Throwable e
+          (.printStackTrace e)))
+      (recur))))
