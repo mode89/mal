ff48ff6f3882583d83e5658128c3fda3cf6f4e07
Andrey Krainyak <mode.andrew@gmail.com>
2023-04-22 11:54:20 +0800

    feat[clj.2]: use eftest test runner

--- impls/clojure.2/test/mal/run_tests.clj
+++ impls/clojure.2/test/mal/run_tests.clj
@@ -1,5 +1,5 @@
 (ns mal.run-tests
-  (:require [clojure.test :refer [run-all-tests]]
+  (:require [eftest.runner :refer [find-tests run-tests]]
             [mal.core-test]
             [mal.lexer-test]
             [mal.parsing-test]
@@ -8,4 +8,11 @@
             [mal.python.compiler-test]))
 
 (defn -main [& _args]
-  (run-all-tests #"mal.*-test"))
+  (run-tests
+    (find-tests
+      ['mal.core-test
+       'mal.lexer-test
+       'mal.parsing-test
+       'mal.printer-test
+       'mal.reader-test
+       'mal.python.compiler-test])))
